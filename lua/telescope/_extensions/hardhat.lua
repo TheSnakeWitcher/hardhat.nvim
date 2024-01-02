5e86ae93c542f37fd0394d939c12cc2f8d26eaae
Mr.PAPI <thesnakewitcher@gmail.com>
2024-01-01 23:47:50 -0500

    feat: add deploy and networks telescope picker

---
+++ lua/telescope/_extensions/hardhat.lua
@@ -0,0 +1,133 @@
+local has_telescope, telescope = pcall(require, "telescope")
+if not has_telescope then
+    vim.notify("hardhat.nvim requires nvim-telescope/telescope.nvim")
+    return
+end
+
+local has_plenary, Job = pcall(require, "plenary.job")
+if not has_plenary then
+    vim.notify("hardhat.nvim requires plenary")
+    return
+end
+
+
+local conf = require("telescope.config").values
+local pickers = require("telescope.pickers")
+local themes = require("telescope.themes")
+local finders = require("telescope.finders")
+local actions = require("telescope.actions") local actions_state = require("telescope.actions.state")
+local actions_utils = require("telescope.actions.utils")
+local make_entry = require("telescope.make_entry")
+
+local hardhat_ignition = require("hardhat.ignition")
+local hardhat_networks = require("hardhat.networks")
+
+
+local hardhat_networks_mappings = function(prompt_bufnr, map)
+    actions.select_default:replace(function()
+        local networks = {}
+        actions_utils.map_selections(prompt_bufnr, function(entry, _)
+            table.insert(networks, entry.value)
+        end)
+	    actions.close(prompt_bufnr)
+
+        if #networks < 1 then
+	        local network = actions_state.get_selected_entry().value
+	        return network
+        else
+	        return networks
+        end
+	end)
+
+    return true
+end
+
+local hardhat_networks_picker_base = function(opts, mappings)
+    opts = opts or {}
+    pickers.new(themes.get_dropdown(), {
+        prompt_title = "hardhat networks",
+        finder = finders.new_table({
+            results = hardhat_networks.get_networks(),
+            entry_maker = make_entry.gen_from_string(opts),
+        }),
+        sorter = conf.generic_sorter(opts),
+        attach_mappings = mappings,
+    }):find()
+end
+
+local hardhat_networks_picker = function(opts)
+    hardhat_networks_picker_base(opts, hardhat_networks_mappings)
+end
+
+local hardhat_deploy_mappings = function(top_prompt_bufnr, map)
+    actions.select_default:replace(function()
+
+	    local deploy_module = actions_state.get_selected_entry().value
+	    vim.notify("deploying " .. deploy_module)
+	    actions.close(top_prompt_bufnr)
+
+        hardhat_networks_picker_base({}, function(prompt_bufnr)
+            actions.select_default:replace(function()
+
+                -- TODO: add support for multiple networks
+                -- local networks = {}
+                -- actions_utils.map_selections(prompt_bufnr, function(entry, _)
+                --     table.insert(networks, entry.value)
+                -- end)
+
+	            local network = actions_state.get_selected_entry().value
+	            actions.close(prompt_bufnr)
+                vim.notify(network)
+
+                Job:new({
+                    command = "pnpm",
+                    args = { "hardhat","ignition", "deploy", deploy_module, "--network", network },
+                    cwd = vim.loop.cwd(),
+                    on_exit = function(job,_)
+                        vim.notify(job:result())
+                    end,
+                    on_stdout = function()
+                        vim.notify("deployment succed")
+                    end,
+                    on_stderr = function()
+                        vim.notify("deployment fails",vim.log.levels.ERROR)
+                    end,
+                }):start()
+            end)
+
+            return true
+        end)
+
+	end)
+    return true
+end
+
+local hardhat_deploy_picker = function(opts)
+    opts = opts or {}
+    pickers.new(opts, {
+        prompt_title = "hardhat ignition deploy modules",
+        finder = finders.new_table({
+            results = hardhat_ignition.list_modules(),
+            entry_maker = make_entry.gen_from_file(opts),
+        }),
+        sorter = conf.file_sorter(opts),
+        previewer = conf.file_previewer(opts),
+        attach_mappings = hardhat_deploy_mappings,
+    }):find()
+end
+
+
+return telescope.register_extension({
+    exports = {
+        deploy = hardhat_deploy_picker,
+        networks = hardhat_networks_picker,
+
+        -- TODO: add pickers for these
+        -- verify = {},
+        -- contracts = {}, -- search contracts with rg
+        -- run = {},       -- search task to run
+        -- export = {},    -- search deployed contracts and verify with hardhat-verify
+        -- sourcify = {},  -- search deployed contracts and verify with sourcify
+    },
+})
+
