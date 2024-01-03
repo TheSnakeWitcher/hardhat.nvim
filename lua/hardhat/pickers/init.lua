6b9a9dcd8a215a26a5825cea949f68e4449f4fea
Mr.PAPI <thesnakewitcher@gmail.com>
2024-01-02 20:08:30 -0500

    ref: create pickers module

---
+++ lua/hardhat/pickers/init.lua
@@ -0,0 +1,10 @@
+local M = {}
+
+
+M.networks = require("hardhat.pickers.networks").hardhat_networks_picker
+M.deploy = require("hardhat.pickers.deploy").hardhat_deploy_picker
+M.deployments = require("hardhat.pickers.deployments").hardhat_deployments_picker
+M.verify = require("hardhat.pickers.verify").hardhat_verify_picker
+
+
+return M
