ec31ea030a16703c6dd61b6c64edeedc4307b9cf
Mr.PAPI <thesnakewitcher@gmail.com>
2024-01-13 16:47:55 -0500

    feat: add hardhat-tests to queries

---
+++ queries/typescript/hardhat-tests.scm
@@ -0,0 +1,53 @@
+;;query describe blocks 
+(( expression_statement
+    ( call_expression
+        function: ( identifier ) @func_id (#eq? @func_id "describe")
+        arguments: ( arguments
+            (string
+                (string_fragment) @namespace.name
+            ) 
+        ) 
+    ) @namespace.definition
+)) 
+
+;;query for it blocks wich use functions
+( (expression_statement
+    (call_expression
+        function: (identifier) @func_id (#eq? @func_id "describe")
+        (arguments (function
+          (statement_block
+            (expression_statement
+                (call_expression
+                    function: (identifier) @test_func_id (#eq? @test_func_id "it")
+                    arguments: (arguments
+                            (string
+                                (string_fragment) @test.name
+                            ) 
+                    ) @test.definition
+                )
+            )
+          )
+        ))
+    )
+) ) 
+
+;;query for it blocks wich use arrow functions
+( (expression_statement
+    (call_expression
+        function: (identifier) @func_id (#eq? @func_id "describe")
+        (arguments (arrow_function
+          (statement_block
+            (expression_statement
+                (call_expression
+                    function: (identifier) @test_func_id (#eq? @test_func_id "it")
+                    arguments: (arguments
+                            (string
+                                (string_fragment) @test.name
+                            ) 
+                    ) @test.definition
+                )
+            )
+          )
+        ))
+    )
+) ) 
