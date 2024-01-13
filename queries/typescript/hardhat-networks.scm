ce7820fce65c7eda86518e3fd75df6e8c9495026
Mr.PAPI <thesnakewitcher@gmail.com>
2024-01-13 16:49:57 -0500

    feat: add hardhat-networks to queries

---
+++ queries/typescript/hardhat-networks.scm
@@ -0,0 +1,14 @@
+;;query networks in hardhat.config.ts
+(( lexical_declaration
+    (variable_declarator
+        (type_annotation
+            (type_identifier) @type (#eq? @type "HardhatUserConfig") 
+        )
+        (object (pair
+            (property_identifier) @prop (#eq? @prop "networks")
+            (object (pair
+                (property_identifier) @networkName
+            )) 
+        ) @networks ) 
+    )
+))
