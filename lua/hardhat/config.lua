7aa335655a7fb2651e488212977aa44a447ab48b
Mr.PAPI <thesnakewitcher@gmail.com>
2024-01-15 13:51:54 -0500

    ref: add namespace to and options check to hardhat config

--- lua/hardhat/config.lua
+++ lua/hardhat/config.lua
@@ -4,11 +4,13 @@ local util = require("hardhat.util")
 local M = {}
 
 
+M.ns = vim.api.nvim_create_namespace("hardhat.nvim")
+
 local defaults = {
     package_manager = util.get_js_package_manager(),
 }
-M.options = {}
 
+M.options = {}
 
 setmetatable(M,{
     __index = function(tbl, k)
@@ -20,8 +22,9 @@ setmetatable(M,{
     end
 })
 
-M.setup = function(opts)
-    M.options = vim.tbl_deep_extend("force", defaults, opts)
+M.setup = function(options)
+    options = options or {}
+    M.options = vim.tbl_deep_extend("force", defaults, options)
 end
 
 
