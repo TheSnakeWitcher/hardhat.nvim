177a401e88a2b1f1bb100255f93618442420ac31
Mr.PAPI <thesnakewitcher@gmail.com>
2024-01-17 21:49:18 -0500

    feat: add support for hardhat-deploy in deployments picker

--- lua/hardhat/pickers/deployments.lua
+++ lua/hardhat/pickers/deployments.lua
@@ -5,22 +5,25 @@ local finders = require("telescope.finders")
 local actions = require("telescope.actions")
 local actions_state = require("telescope.actions.state")
 
+local util = require("hardhat.util")
 local hardhat_ignition = require("hardhat.ignition")
+local hardhat_deploy = require("hardhat.deploy")
 
 
 local M = {}
 
 
-M.hardhat_deployments_picker_base = function(opts, mappings)
-    opts = opts or {}
-    pickers.new(themes.get_dropdown(), {
-        prompt_title = "hardhat ignition deployments",
+M.hardhat_deployments_picker_base = function(opts, deployment_finder, mappings)
+    opts = vim.tbl_deep_extend("force", opts or {}, themes.get_dropdown())
+
+    pickers.new( opts , {
+        prompt_title = "hardhat deployments",
         finder = finders.new_table({
-            results = hardhat_ignition.get_deployments(),
+            results = deployment_finder(),
             entry_maker = function(entry)
                 return {
                     value = entry,
-                    display = string.format(" %s %s: %s",entry.chain_id, entry.deployment_id, entry.address),
+                    display = string.format(" %s %s: %s", entry.chain_id, entry.deployment_id, entry.address),
                     ordinal = entry.chain_id
                 }
             end,
@@ -30,15 +33,32 @@ M.hardhat_deployments_picker_base = function(opts, mappings)
     }):find()
 end
 
+
 M.hardhat_deployments_picker = function(opts)
-    M.hardhat_deployments_picker_base(opts, function(prompt_bufnr)
-        actions.select_default:replace(function()
-            local deployment = actions_state.get_selected_entry()
-            vim.notify(string.format("deployment id: %s", deployment.value.deployment_id))
-            actions.close(prompt_bufnr)
-        end)
-        return true
-    end)
+    opts = opts or {}
+    util.check_deploy_system_and_do(
+        function()
+            M.hardhat_deployments_picker_base(opts, hardhat_ignition.get_deployments, function(prompt_bufnr)
+                actions.select_default:replace(function()
+                    local deployment = actions_state.get_selected_entry()
+                    vim.notify(string.format("deployment id: %s", deployment.value.deployment_id))
+                    actions.close(prompt_bufnr)
+                end)
+                return true
+            end)
+        end,
+        function()
+            M.hardhat_deployments_picker_base(opts, hardhat_deploy.get_deployments, function(prompt_bufnr)
+                actions.select_default:replace(function()
+                    local deployment = actions_state.get_selected_entry()
+                    vim.notify(string.format("deployment id: %s", deployment.value.deployment_id))
+                    actions.close(prompt_bufnr)
+                end)
+                return true
+            end)
+
+        end
+    )
 end
 
 
