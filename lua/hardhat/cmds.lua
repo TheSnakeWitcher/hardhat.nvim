c8dd73042736e024a2c9ce3fa46974bef4578692
Mr.PAPI <thesnakewitcher@gmail.com>
2024-01-31 18:42:48 -0500

    style: renma hh_cli to cli in hardhat.cmds module

--- lua/hardhat/cmds.lua
+++ lua/hardhat/cmds.lua
@@ -1,13 +1,14 @@
-local hh_cli = require('hardhat.cli')
+local cli = require('hardhat.cli')
+
 
 vim.api.nvim_create_user_command(
-    hh_cli.CMD,
-    function(opts) hh_cli.run(opts) end,
+    cli.CMD,
+    function(opts) cli.run(opts) end,
     {
         --- @help command-attributes
         nargs = '*',
         complete = function(arglead, cmdline, cursor_pos)
-            return hh_cli.complete(arglead, cmdline, cursor_pos)
+            return cli.complete(arglead, cmdline, cursor_pos)
         end,
     }
 )
