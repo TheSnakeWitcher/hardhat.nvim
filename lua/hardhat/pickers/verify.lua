b36116ef18839b6a12cc077dfc67630b8f85b74c
Mr.PAPI <thesnakewitcher@gmail.com>
2024-02-03 15:03:25 -0500

    ref: use picker_util.do_with_pairs in verify picker
    Only available for now to hardhat-deploy verify pickers because there is
    no current way to get the network name for hardhat-ignition

--- lua/hardhat/pickers/verify.lua
+++ lua/hardhat/pickers/verify.lua
@@ -5,6 +5,7 @@ local Job = require("plenary.job")
 
 local config = require("hardhat.config")
 local util = require("hardhat.util")
+local pickers_util = require("hardhat.util.pickers")
 local hardhat_ignition = require("hardhat.ignition")
 local hardhat_deploy = require("hardhat.deploy")
 local hardhat_deployments_picker_base = require("hardhat.pickers.deployments").hardhat_deployments_picker_base
@@ -17,15 +18,12 @@ local M = {}
 local function hardhat_verify_ignition_picker(opts)
     hardhat_networks_picker_base({}, function(top_prompt_bufnr)
         actions.select_default:replace(function()
-            local network = actions_state.get_selected_entry().value
-	        actions.close(top_prompt_bufnr)
 
-            hardhat_deployments_picker_base(opts, hardhat_ignition.get_deployments, function(prompt_bufnr)
+            local network = pickers_util.get_entry_and_close_buf(top_prompt_bufnr)
+            hardhat_deployments_picker_base(opts, hardhat_ignition.get_deployments, function(top_prompt_bufnr)
                 actions.select_default:replace(function()
 
-                    local deployment_id = actions_state.get_selected_entry().value.deployment_id
-	                actions.close(prompt_bufnr)
-
+                    local deployment_id = pickers_util.get_entry_and_close_buf(top_prompt_bufnr).deployment_id
 	                vim.notify(string.format("verifying %s using netowrk %s", deployment_id, network))
                     Job:new({
                         command = config.package_manager, args = { "hardhat", "--network", network, "ignition", "verify", deployment_id },
@@ -33,41 +31,41 @@ local function hardhat_verify_ignition_picker(opts)
                         on_exit = function(job,_) vim.notify(job:result()) end,
                     }):start()
                 end)
+
                 return true
-
             end)
-
         end)
+
         return true
     end)
-
 end
 
 
 local function hardhat_verify_deploy_picker(opts)
     hardhat_networks_picker_base({}, function(top_prompt_bufnr)
         actions.select_default:replace(function()
-            local network = actions_state.get_selected_entry().value
-	        actions.close(top_prompt_bufnr)
 
-            hardhat_deployments_picker_base(opts, { hardhat_deploy.get_chain_deployments, network }, function(prompt_bufnr)
+            local selected_network = pickers_util.get_entry_and_close_buf(top_prompt_bufnr)
+            hardhat_deployments_picker_base(opts, { hardhat_deploy.get_chain_deployments, selected_network }, function(prompt_bufnr)
                 actions.select_default:replace(function()
-                    local deployment = actions_state.get_selected_entry().value
-	                actions.close(prompt_bufnr)
 
-	                vim.notify(string.format("verifying %s using netowrk %s", deployment.deployment_id, network))
-                    Job:new({
-                        command = config.package_manager,
-                        args = vim.list_extend({ "hardhat", "--network", network, "verify", deployment.address }, deployment.args ),
-                        cwd = util.get_root(),
-                        on_exit = function(job,_) vim.notify(job:result()) end,
-                    }):start()
+                    local deployments = pickers_util.get_entries_and_close_buf(prompt_bufnr)
+                    pickers_util.do_with_pairs({ selected_network }, deployments, function(network, deployment)
+	                    vim.notify(string.format("verifying %s using netowrk %s", deployment.deployment_id, network))
+                        Job:new({
+                            command = config.package_manager,
+                            args = vim.list_extend({ "hardhat", "--network", network, "verify", deployment.address }, deployment.args ),
+                            cwd = util.get_root(),
+                            on_exit = function(job,_) vim.notify(job:result()) end,
+                        }):start()
+                    end)
+
                 end)
 
                 return true
             end)
-
         end)
+
         return true
     end)
 end
